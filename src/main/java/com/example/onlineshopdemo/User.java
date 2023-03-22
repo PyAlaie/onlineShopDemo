1ac34e06e37164db5c01643b2450c8c4ce8a391b
Arshia Alaie <arshiaalaie1@gmail.com>
2023-03-22 18:23:27 +0330

    add setter and getter to User

--- src/main/java/com/example/onlineshopdemo/User.java
+++ src/main/java/com/example/onlineshopdemo/User.java
@@ -15,6 +15,30 @@ public class User {
         this.role = role;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
     public User(String username, String password, String email) {
         this.username = username;
         this.password = password;
