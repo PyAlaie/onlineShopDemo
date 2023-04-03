1f378a4f7dea223d75d65a7e7de9125e48d2105d
Arshia Alaie <arshiaalaie1@gmail.com>
2023-04-03 09:29:51 +0330

    Starting the UI

--- src/main/java/com/example/onlineshopdemo/HelloApplication.java
+++ src/main/java/com/example/onlineshopdemo/HelloApplication.java
@@ -6,22 +6,30 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/onlineshopdemo/loginRegister.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginRegister.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        OnlineShop.setName("KOKOKALA");
-        OnlineShop.setWebAddress("kokokala.com");
-        OnlineShop.setSupportPhoneNumber("123456789");
-        Test.importTestCases();
-
+        if(!OnlineShop.tryToLoad()){
+            OnlineShop.setName("KOKOKALA");
+            OnlineShop.setWebAddress("kokokala.com");
+            OnlineShop.setSupportPhoneNumber("123456789");
+            Test.importTestCases();
+            System.out.println("we are doin it all over again");
+        }
         launch();
+    }
+    @Override
+    public void stop(){
+        OnlineShop.save();
     }
 }
